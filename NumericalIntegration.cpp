c442da603463abe1127d8510e7dd2f5f8e4be783
akash07013 <141671762+akash07013@users.noreply.github.com>
2024-11-20 12:26:37 +0600

    Update NumericalIntegration.cpp

--- NumericalIntegration.cpp
+++ NumericalIntegration.cpp
@@ -19,7 +19,7 @@ double trapezoidal_rule(double a,double b,int n) {
 
 double simpson_1_3_rule(double a,double b,int n) {
     if (n%2!=0){
-        cerr<<"Simpson's 1/3 rule requires an even number of subintervals!"<<endl;
+        cout<<"Simpson's 1/3 rule requires an even number of subintervals!"<<endl;
         return -1; 
     }
 
@@ -34,7 +34,7 @@ double simpson_1_3_rule(double a,double b,int n) {
 
 double simpson_3_8_rule(double a, double b, int n) {
     if (n%3!=0){
-        cerr<<"Simpson's 3/8 rule requires the number of subintervals to be a multiple of 3"<<endl;
+        cout<<"Simpson's 3/8 rule requires the number of subintervals to be a multiple of 3"<<endl;
         return -1;
     }
 
